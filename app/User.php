bb4438578604722a2670feada9e67bc3baf46c6a
webstar1987923 <webstar1987923@hotmail.com>
2018-02-22 17:49:11 +0300

    fixed country issue

--- app/User.php
+++ app/User.php
@@ -13,7 +13,7 @@ class User extends Model
      * @var array
      */
     protected $fillable = [
-        'first_name', 'last_name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password', 'country',
     ];
 
     /**
